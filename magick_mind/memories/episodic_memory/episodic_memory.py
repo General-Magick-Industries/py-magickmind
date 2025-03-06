62b2ef823cb4909b95f1fcad660b52201d6dee38
mintea3 <minthu@globalmagicko.com>
2025-03-07 01:18:57 +0630

    Update episodic_memory.py
    Change episodic_memory.py to test automated formatting

--- magick_mind/memories/episodic_memory/episodic_memory.py
+++ magick_mind/memories/episodic_memory/episodic_memory.py
@@ -27,11 +27,7 @@ class EpisodicMemory:
             self.collection.create_index(
                 [("embedding", "vectorSearch")],
                 {
-                    "vectorSearchOptions": {
-                        "numDimensions": 1536,
-                        "similarity": "cosine",
-                    }
-                },
+                    "vectorSearchOptions": {"numDimensions": 1536,"similarity": "cosine"}},
             )
 
     def get_embedding(self, query: str) -> List[float]:
